14fee3faeebfcca1d56ff99f1e6162d857d59e0d
TheCodeNinja <turtlebait13@gmail.com>
2016-10-31 13:39:28 -0400

    test environment var

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import os
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! " + os.environ['MYVAR']
 
 if __name__ == "__main__":
     application.run()
